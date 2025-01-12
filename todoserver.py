88ebdbc8010a61d04c3e5a531ed1d01c620e5cca
Gayathri <gayathri@Gayathris-MacBook-Air.local>
2025-01-12 11:33:33 -0500

    .

--- todoserver.py
+++ todoserver.py
@@ -1,16 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 todoList = { "monday": ["Buy groceries", "Clean the house"] }
 
 app = Flask(__name__)
 
-@app.route("/todos", methods=["GET"])
-def getlist():
-  return todoList
-
-@app.route("/todos", methods=["POST"])
-def create():
-    return "Create a new todo item"
+@app.route("/todos", methods=["GET","POST"])
+def todos():
+  if request.method == "GET":
+    return todoList
+  elif request.method == "POST":
+    data = request.get_json()
+    print(str(data))
+    return "",200
+  return 400
   
 
 if __name__ == "__main__":
